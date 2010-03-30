f99460a57360ba1149971f64d24b22751d7222bb
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-03-29 21:30:55 -0600

    Committing exercise 1.3 from SICP.

--- chap1/ex_1_3.clj
+++ chap1/ex_1_3.clj
@@ -2,15 +2,15 @@
 
 (defn square[x] (* x x))
 
-(defn sum-of-square[x y] (+ (square x) (square y)))
+(defn sum-of-squares[x y] (+ (square x) (square y)))
 
 (defn square-biggest-two [a b c]
-  (let [x (sum-of-square a b)
-        y (sum-of-square a c)
-        z (sum-of-square b c)]
-    (cond (and (> x y) (> x z)) x
-      (and (> y x) (> y z)) y
-      :else z
-      )))
+    (cond (> a b c) (sum-of-squares a b)
+	  (> b c a) (sum-of-squares b c)
+	  (> c a b) (sum-of-squares c a)
+	  (> c b a) (sum-of-squares c b)
+	  (> b a c) (sum-of-squares b a)
+	  (> a c b) (sum-of-squares a c)))
 
-(square-biggest-two 1 2 3)
+(square-biggest-two 1 2 3)  ;13
+(square-biggest-two 1 2 -3 );5
