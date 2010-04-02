c08e450e1320a490e5f5429a2a556261ed96a6ae
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-04-01 23:17:01 -0600

    Adding iterative solution.

--- chap1/ex_1_11.clj
+++ chap1/ex_1_11.clj
@@ -9,6 +9,22 @@
        (* (ex111 (- n 2)) 2)
        (* (ex111 (- n 3)) 3))))
 
-(ex111 5)
+(ex111 3)
+(map #(ex111 %) (range 0 10))
+;(0 1 2 4 11 25 59 142 335 796)
 
-;Don't know how to do iterative solution
+;From "Programming Clojure" Chapter 5
+(defn fibo[]
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
+(take 20 (fibo))
+
+;super-slick iterative solution
+(defn ex111-iter[]
+  (map first (iterate
+	      (fn [[a b c]]
+		[b c (+ c (* 2 b) (* 3 a))])
+	      [0 1 2])))
+
+(take 10 (ex111-iter))
+;(0 1 2 4 11 25 59 142 335 796)
