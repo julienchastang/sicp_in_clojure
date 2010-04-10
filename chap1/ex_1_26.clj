37f395aea6e3875b070b1e3803ef03e29a49dba2
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-04-10 10:02:06 -0600

    Better answer hopefully.

--- chap1/ex_1_26.clj
+++ chap1/ex_1_26.clj
@@ -2,4 +2,4 @@
   (:use clojure.contrib.math))
 
 ; Eva Lu Ator is right. Louis Reasoner is unnecessarily computing expmod twice
-; at each iteration.
+; at each iteration. This leads to a process that is 2^(log2n) === n.
