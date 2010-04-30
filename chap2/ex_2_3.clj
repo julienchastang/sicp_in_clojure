3a924bcbc860cfcbb816363caab3fc1da37441c1
Julien Chastang <julien.c.chastang@gmail.com>
2010-04-30 16:53:40 -0600

    Committing SICP exercise 2.3.

---
+++ chap2/ex_2_3.clj
@@ -0,0 +1,40 @@
+(ns sicp-in-clojure.chap1.ex-2-3
+  (:use clojure.contrib.math))
+
+(defn make-point[x y]
+  (list x y))
+
+(defn make-rectangle[x y w h]
+  (list (make-point x y) w h))
+
+(defn perimeter[r]
+  ( + (* (second r) 2) (* (last r) 2)))
+
+(defn area[r]
+  (* (second r) (last r)))
+
+(def a (make-rectangle  2 3 4 5))
+
+(perimeter a)
+
+(area a)
+
+; Here is another representation.
+
+(defn make-rectangle[x y w h]
+  (list (make-point x y) w h
+	( + (* w 2) (* h 2))
+	(* w h)))
+
+(defn perimeter[r]
+     (nth r 3))
+
+(defn area[r]
+  (nth r 4))
+
+(def a (make-rectangle  2 3 4 5))
+
+(perimeter a)
+
+(area a)
+
