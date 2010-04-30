759d3aea9aa828682ef48b5d20ec5eaebaf6c116
Julien Chastang <julien.c.chastang@gmail.com>
2010-04-29 22:55:44 -0600

    Committing SICP exercise 2.2.

---
+++ chap2/ex_2_2.clj
@@ -0,0 +1,32 @@
+(ns sicp-in-clojure.chap1.ex-2-2
+  (:use clojure.contrib.math))
+
+(defn average[coll]
+  (let [cnt (count coll)]
+    (reduce + (map #(/ % cnt) coll))))
+
+(defn make-point[x y]
+  (list x y))
+
+(defn x-point[p]
+  (first p))
+
+(defn y-point[p]
+  (last p))
+
+(defn make-segment[& p]
+  (apply list p))
+
+(defn start-segment[s]
+  (first s))
+
+(defn end-segment[s]
+  (last s))
+
+(defn midpoint-segment[s]
+  (make-point (average (map #(x-point %) s))
+	      (average (map #(y-point %) s))))
+
+(def a (make-segment (make-point 1 1) (make-point 2 2) (make-point 3 3)))
+
+(midpoint-segment a)
