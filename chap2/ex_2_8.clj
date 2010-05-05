2691a148c2c370ad5d3ed94897a26817a04ff12c
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-04 23:49:08 -0600

    Committing SICP exercise 2.8.

---
+++ chap2/ex_2_8.clj
@@ -0,0 +1,17 @@
+(ns sicp-in-clojure.chap1.ex-2-8
+  (:use clojure.contrib.math))
+
+(defn lower-bound[x] (min x))
+
+(defn upper-bound[x] (max x))
+
+(defn make-interval [a b] (list a b)) 
+
+(defn add-interval [x y]
+  (make-interval (+ (lower-bound x) (lower-bound y))
+  		 (+ (upper-bound x) (upper-bound y))))
+
+(defn sub-interval [x y]
+  (make-interval (- (upper-bound x) (lower-bound y))
+  		 (- (lower-bound x) (upper-bound y))))
+
