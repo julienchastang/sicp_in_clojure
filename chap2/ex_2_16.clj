0e6e74b3eed1f03c1b4aed2ac7cf1298df8fbbb2
Julien Chastang <julien.c.chastang@gmail.com>
2010-05-11 22:18:20 -0600

    Committing SICP exercise 2.16.

---
+++ chap2/ex_2_16.clj
@@ -0,0 +1,4 @@
+; Another reason algebraic expressions may lead to different answer may be associated with IEEE floating
+; point arithmetic. IEEE floating point math is notoriously bad at dealing with decimal arithmetic for
+; currency calculations for example. Or the floating point arithmetic can lead to problems where the
+; calculations are "ill-conditioned" because of very small or very  large values.
