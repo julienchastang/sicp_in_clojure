802bf3a5a3c7702fc325d78d791efd4b02cc8af5
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-11 23:13:10 -0600

    Committing SICP exercise 2.18.

---
+++ chap2/ex_2_18.clj
@@ -0,0 +1,10 @@
+(ns sicp-in-clojure.chap1.ex-2-18
+  (:use clojure.contrib.math))
+
+(defn my-reverse[c]
+  (loop [col c l '()]
+    (if (= (count col) 0)
+      l
+      (recur (rest col) (cons (first col) l)))))
+
+(my-reverse (list 1 4 9 16 25))
