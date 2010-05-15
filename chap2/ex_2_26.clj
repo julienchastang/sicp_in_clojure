9c4cf1b8ababa04035a0632f4c1aeb34124f58b9
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-15 13:29:11 -0600

    Committing SICP exercise 2.26.

---
+++ chap2/ex_2_26.clj
@@ -0,0 +1,15 @@
+(ns sicp-in-clojure.chap1.ex-2-26
+  (:use clojure.contrib.repl-utils))
+
+(def x (list 1 2 3))
+
+(def y (list 4 5 6))
+
+(concat x y)
+; (1 2 3 4 5 6)
+
+(cons x y)
+; ((1 2 3) 4 5 6)
+
+(list x y)
+; ((1 2 3) (4 5 6))
