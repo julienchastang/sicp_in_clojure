bd648b888e35373d6a5735a2411a15b3d25c36b3
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-15 13:28:11 -0600

    Committing SICP exercise 2.25.

---
+++ chap2/ex_2_25.clj
@@ -0,0 +1,14 @@
+(ns sicp-in-clojure.chap1.ex-2-25
+  (:use clojure.contrib.repl-utils))
+
+(def a '(1 3 (5 7) 9))
+(first (rest (first (rest (rest a)))))
+
+(def b '((7)))
+(first (first b))
+
+(def c '(1 (2 (3 (4 (5 (6 7)))))))
+(first (rest (first (rest (first (rest (first (rest (first (rest (first (rest c))))))))))))
+
+
+
