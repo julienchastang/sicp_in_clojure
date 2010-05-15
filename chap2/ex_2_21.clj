c477074a1b35ac666e551e7f8cd53a27c69ab6e0
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-14 22:14:21 -0600

    Committing SICP exercise 2.21.

---
+++ chap2/ex_2_21.clj
@@ -0,0 +1,17 @@
+(ns sicp-in-clojure.chap1.ex-2-21
+  (:use clojure.contrib.repl-utils))
+
+(defn square[x] (* x x))
+
+(defn square-list[items]
+  (if (empty? items)
+    nil
+    (cons (square (first items))
+	  (square-list (rest items)))))
+
+(square-list (list 1 2 3 4))
+
+(defn square-list[items]
+  (map #(square %) items))
+
+(square-list (list 1 2 3 4))
