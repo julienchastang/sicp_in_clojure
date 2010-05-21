57633472f38f3b13ed3d84a6e0ae5e55e5231f5b
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-20 21:35:39 -0600

    Committing SICP exercise 2.28.

---
+++ chap2/ex_2_28.clj
@@ -0,0 +1,10 @@
+(ns sicp-in-clojure.chap1.ex-2-28
+  (:use clojure.contrib.repl-utils))
+
+(defn fringe[x]
+  (cond (not (list? x)) (list x)
+	(empty? x) x
+	:else (concat (fringe (first x))
+		      (fringe (rest x)))))
+
+(fringe '(1 2 (1 (3 2 1) 2) (7 8 9)))
