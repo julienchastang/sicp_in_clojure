0c61dc62c3f85452629f77318d05e82955e347e6
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-21 22:41:57 -0600

    Committing SICP exercise 2.31.

---
+++ chap2/ex_2_31.clj
@@ -0,0 +1,17 @@
+(ns sicp-in-clojure.chap1.ex-2-31
+  (:use clojure.contrib.repl-utils))
+
+(defn square[x] (* x x))
+
+(def tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+
+(defn tree-map[f x]
+  (cond (not (list? x)) (f x)
+	(empty? x) x
+	:else (cons (tree-map f (first x))
+		    (tree-map f (rest x)))))
+
+(defn square-tree[tree]
+  (tree-map square tree))
+
+(square-tree tree)
