d5911934734d03de1bb28f0874e32be41cff05e8
Julien Chastang <julien.c.chastang@gmail.com>
2010-05-26 17:19:33 -0600

    Committing SICP exercise 2.34.

---
+++ chap2/ex_2_34.clj
@@ -0,0 +1,23 @@
+(ns sicp-in-clojure.chap1.ex-2-34
+  (:use clojure.contrib.repl-utils))
+
+(let [x (ref -1)]
+  (defn incr1 []
+    (dosync (ref-set x (inc @x)))))
+
+(defn accumulate [op initial sequence]
+  (if (empty? sequence) initial
+      (op (first sequence)
+	  (accumulate op initial (rest sequence)))))
+
+(defn horner-eval[y coefficient-sequence]
+  (accumulate (fn[this-coeff higher-terms]
+		(+ (* this-coeff (Math/pow y (incr1)))
+		   higher-terms))
+	      0
+	      (reverse coefficient-sequence)))
+
+
+(def a (horner-eval 2 (list 1 3 0 5 0 1)))
+a
+
