8b32423114f18793c47177341c3f077cd18114f6
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2010-05-28 15:20:17 -0600

    With Tyler's help, fixed.

--- chap2/ex_2_6.clj
+++ chap2/ex_2_6.clj
@@ -1,9 +1,33 @@
 (ns sicp-in-clojure.chap1.ex-2-6
   (:use clojure.contrib.math))
 
-(def zero (fn[f] (fn[x] x)))
+(def zero (fn [f] identity))
 
-((zero 1) 1)
+(def one identity)
+
+(def two (fn [f] (comp f f)))
+
+(def three (fn [f] (comp f f f)))
+
+(defn church-num [n] ((n inc) 0))
+
+(defn add-1[n]
+    (fn[f] (fn[x] (f ((n f) x)))))
+
+(church-num zero) ;0
+
+(church-num one) ;1
+
+(church-num two) ;2
+
+(church-num three) ;3
+
+(church-num (add-1 zero)) ;1
+
+
+
+
+
 
 
 
