7747b8caf6a3920683a74c42915c9c185e010b2f
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2011-01-15 14:55:18 -0700

    Trying to move to Leiningen.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject sicp_in_clojure "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :disable-implicit-clean true)
---
+++ test/sicp_in_clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns sicp_in_clojure.test.core
+  (:use [sicp_in_clojure.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
