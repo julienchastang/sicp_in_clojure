576c05b1c0ad544a474fa3d9738fc25fbe49d2aa
Julien Chastang <julien.c.chastang@gmail.com>
2011-01-19 23:38:56 -0700

    SICP 2.38

---
+++ src/sicp_in_clojure/chap2/ex_2_38.clj
@@ -0,0 +1,27 @@
+(ns sicp-in-clojure.chap2.ex-2-38
+  (:use clojure.contrib.repl-utils))
+
+(defn fold-right [op initial sequence]
+  (if (empty? sequence) initial
+      (op (first sequence)
+	  (fold-right op initial (rest sequence)))))
+
+(defn fold-left [op initial sequence]
+  (letfn [(iter [result rst]
+		(if (empty? rst)
+		  result
+		  (iter (op result (first rst))
+			(rest rst))))]
+    (iter initial sequence)))
+
+(fold-right / 1 (vector 1 2 3))
+
+(fold-left / 1 (vector 1 2 3))
+
+(fold-right vector nil (vector 1 2 3))
+
+(fold-left vector nil (vector 1 2 3))
+
+(fold-right + 1 (vector 1 2 3))
+
+(fold-left + 1 (vector 1 2 3))
