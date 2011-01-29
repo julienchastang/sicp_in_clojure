b46cb05790ea5d4b0c158c0210dde3af481b2c02
Julien Chastang <julien.c.chastang@gmail.com>
2011-01-29 00:02:17 -0700

    SICP 2.41

---
+++ src/sicp_in_clojure/chap2/ex_2_41.clj
@@ -0,0 +1,42 @@
+(ns sicp-in-clojure.chap2.ex-2-41
+  (:use clojure.contrib.repl-utils)
+  (:require [sicp-in-clojure.chap1.ex-1-22 :as ex22])
+  (:require [sicp-in-clojure.chap2.ex-2-33 :as ex33]))
+
+(defn accumulate [op initial sequence]
+  (if (empty? sequence) initial
+      (op (first sequence)
+	  (accumulate op initial (rest sequence)))))
+
+(defn prime? [n]
+  (:result (ex22/prime? n)))
+
+(defn flatmap [proc seq]
+  (accumulate ex33/append nil (map proc seq)))
+
+(defn prime-sum? [triple]
+  (prime? (+ (first triple)
+	     (second triple)
+	     (last triple))))
+
+(defn make-triple-sum [triple]
+  (list (first triple)
+	(second triple)
+	(last triple)
+	(+ (first triple)
+	   (second triple)
+	   (last triple))))
+
+(defn unique-triples [n]
+  (flatmap (fn [i]
+	     (flatmap 
+	      (fn [j]
+		(map
+		 (fn [k] (list i j k))
+		 (range 1 j)))
+	      (range 1 i)))
+	   (range 1 (inc n))))
+
+(defn prime-sum-triples [n]
+  (map make-triple-sum
+       (filter prime-sum? (unique-triples n))))
