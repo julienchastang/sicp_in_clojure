0096ac00eb6862e7ab653f0214e7c25211ad2e49
Julien Chastang <julien.c.chastang@gmail.com>
2011-02-21 22:50:38 -0700

    Adding iText stuff.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject sicp_in_clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [com.lowagie/itext "2.0.7"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :disable-implicit-clean true)
