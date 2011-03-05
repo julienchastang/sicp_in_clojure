076a920e967516054f685989b026cdf5afe92606
Julien Chastang <julien.c.chastang@gmail.com>
2011-03-04 23:52:06 -0700

    2.48

---
+++ src/sicp_in_clojure/chap2/ex_2_48.clj
@@ -0,0 +1,60 @@
+(ns sicp-in-clojure.chap2.ex-2-48
+  (:use [sicp-in-clojure.chap2.ex-2-46])
+  (:import (com.lowagie.text Document Paragraph)
+	   (com.lowagie.text.pdf PdfWriter PdfContentByte)
+	   (java.io FileOutputStream)))
+
+(defn draw [segs]
+  (let [my-doc (Document.)
+	writer (PdfWriter/getInstance my-doc
+				      (FileOutputStream.
+				       "/tmp/draw.pdf"))
+	cb (do (.open my-doc)
+	       (.getDirectContent writer))]
+    (try (doall (map (fn [[[x0 y0] [x1 y1]]] 
+		       (do (.moveTo cb x0 y0)
+			   (.lineTo cb x1 y1))) segs))
+	 (.stroke cb)
+	 (finally (.close my-doc)))))
+
+(def wave [[ [0.13 0.6] [0.3 0.66]]
+	   [ [0.3 0.66] [0.4 0.66]]
+	   [ [0.4 0.66] [0.36 0.84]]
+	   [ [0.36 0.84] [0.4 1]]
+	   [ [0.6 1] [0.64 0.84]]
+	   [ [0.64 0.84] [0.6 0.66]]
+	   [ [0.6 0.66] [0.71 0.66]]
+	   [ [0.71 0.66] [1 0.34]]
+	   [ [1 0.16] [0.6 0.44]]
+	   [ [0.6 0.44] [0.74 0]]
+	   [ [0.6 0] [0.5 0.3]]
+	   [ [0.5 0.3] [0.4 0]]
+	   [ [0.24 0] [0.34 0.5]]
+	   [ [0.34 0.5] [0.3 0.6]]
+	   [ [0.3 0.6] [0.14 0.4]]
+	   [ [0.14 0.4] [0 0.64]]])
+
+(defn make-segment [[ x0 y0 x1 y1]]
+  [[x0 y0] [x1 y1]])
+
+(defn start-segment[segment]
+  (first segment))
+
+(defn end-segment[segment]
+  (last segment))
+
+
+(defn segments->painters [segment-list]
+  (fn [frame]
+    (map (fn [segment]
+	   [((frame-coord-map frame) (start-segment segment))
+	    ((frame-coord-map frame) (end-segment segment))])
+	 segment-list)))
+
+(def a (segments->painters wave))
+
+
+(def b (a (make-frame [100 100] [100 0] [0 100])))
+
+
+(draw b)
