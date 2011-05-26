2edf4330c5d4fde1aaa1df44a0e5d1256af5e0e3
julien.c.chastang@gmail.com <julien.c.chastang@gmail.com>
2011-05-26 17:47:08 -0600

    Adding midje.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [com.lowagie/itext "2.0.7"]]
+		 [com.lowagie/itext "2.0.7"]
+		 [midje "1.1-RC1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :disable-implicit-clean true)
