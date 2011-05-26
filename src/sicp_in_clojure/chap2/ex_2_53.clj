767821a0e0193509d0525a5257cd8c7a1f5dd9b1
Julien Chastang <julien.c.chastang@gmail.com>
2011-05-26 17:48:50 -0600

    Exercise 2.53

---
+++ src/sicp_in_clojure/chap2/ex_2_53.clj
@@ -0,0 +1,39 @@
+(ns sicp-in-clojure.chap1.ex-2-53
+  (:use clojure.test)
+  (:use midje.sweet))
+
+(fact
+ (list 'a 'b 'c) =>
+ '(a b c))
+
+(fact
+ (list (list 'george)) =>
+ '((george)))
+
+(fact
+ (rest '((x1 x2) (y1 y2)))
+ '(y1 y2))
+
+(defn memq [item x]
+  (cond (empty? x) false
+	(= item (first x)) x
+	:else (memq item (rest x))))
+
+(fact
+ (memq 'red '((red shoes) (blue socks))) =>
+ false)
+
+(fact
+ (memq 'red '(red shoes blue socks))
+ '(red shoes blue socks))
+
+
+(defn my-equal? [a b]
+  (if (or (empty? a) (empty? b)) false
+      (and (= (first a) (first b))
+	   (my-equal? (rest a) (rest b)))))
+
+(fact
+ (my-equal? '(this is  a list)
+	    '(this (is a) list)) =>
+ false) 
