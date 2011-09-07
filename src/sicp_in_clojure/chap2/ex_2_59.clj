c6fde3ed155c9e532b0e35034a562619dddcf47f
Julien Chastang <julien.c.chastang@gmail.com>
2011-09-06 23:04:28 -0600

    exercise 2.59

---
+++ src/sicp_in_clojure/chap2/ex_2_59.clj
@@ -0,0 +1,46 @@
+(ns sicp-in-clojure.chap1.ex-2-59)
+
+(defn element-of-set? [x set]
+  (cond (nil? set) false
+	(= x (first set)) true
+	:else (element-of-set? x (next set))))
+
+
+(defn adjoin-set [ x set]
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+
+(defn intersection-set [set1 set2]
+  (cond (or (nil? set1) (nil? set2)) '()
+	(element-of-set? (first set1) set2)
+	(cons (first set1)
+	      (intersection-set (next set1) set2)) 
+	:else (intersection-set (next set1) set2)))
+
+(defn union-set [set1 set2]
+  (cond (and (not (nil? set1)) (not (nil? set2))) (adjoin-set (first set1) set2)
+	(nil? set1) set2
+	(nil? set2) set1
+	:else (union-set (next set1) set2)))
+
+
+(defn union-set [set1 set2]
+  (cond (and (nil? set1) (nil? set2)) nil
+	(nil? set2) set1
+	(nil? set1) set2
+	:else (union-set (next set1)
+			 (adjoin-set (first set1) set2))))
+
+
+(def a (reduce #(adjoin-set %2 %1) '() (range 3 7)))
+
+(def b (reduce #(adjoin-set %2 %1) '() (range 5 10)))
+
+
+(intersection-set a b)
+;; (6 5)
+
+(union-set a b)
+
+;; (3 4 9 8 7 6 5)
