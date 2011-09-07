2821809f897dbe04c9a1d945e75f7172b584a475
Julien Chastang <julien.c.chastang@gmail.com>
2011-09-07 00:06:48 -0600

    Ex 2.60

---
+++ src/sicp_in_clojure/chap2/ex_2_60.clj
@@ -0,0 +1,39 @@
+(ns sicp-in-clojure.chap1.ex-2-60)
+
+(defn element-of-set? [x set]
+  (cond (nil? set) false
+	(= x (first set)) true
+	:else (element-of-set? x (next set))))
+
+(defn adjoin-set [x set]
+  (cons x set))
+
+(defn intersection-set [set1 set2]
+  (cond (or (nil? set1) (nil? set2)) '()
+	(element-of-set? (first set1) set2)
+	(cons (first set1)
+	      (intersection-set (next set1) set2)) 
+	:else (intersection-set (next set1) set2)))
+
+(defn union-set [set1 set2]
+  (cond (and (nil? set1) (nil? set2)) nil
+	(nil? set2) set1
+	(nil? set1) set2
+	:else (union-set (next set1)
+			 (adjoin-set (first set1) set2))))
+
+
+(def a (reduce #(adjoin-set %2 %1) '() (concat (range 3 7) (range 5))))
+
+(def b (reduce #(adjoin-set %2 %1) '() (concat (range 5 10) (range 7))))
+
+
+(intersection-set a b)
+
+(union-set a b)
+
+;; adjoin-set is now constant time since it no longer depends on element-of-set?
+
+;; intersection-set stays O(n^2)
+
+;; union-set is time o(n)
