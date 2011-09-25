fb7d33ac91265f75f13e7ff3cd873d1a36117366
Julien Chastang <julien.c.chastang@gmail.com>
2011-09-24 23:27:16 -0600

    Forgot to deal with duplicates.

--- src/sicp_in_clojure/chap2/ex_2_62.clj
+++ src/sicp_in_clojure/chap2/ex_2_62.clj
@@ -9,10 +9,15 @@
 	(empty? set1) set2
         (< (first set1) (first set2))
         (cons (first set1) (union-set (next set1) set2))
+        (= (first set1) (first set2))
+        (cons (first set1) (union-set (next set1) (next set2)))
 	:else (cons (first set2) (union-set set1 (next set2)))))
 
 (fact (union-set '(1 3) '(2 4)) =>
       '(1 2 3 4))
+
+(fact (union-set '(1 3) '(1 4)) =>
+      '(1 3 4))
 
 (fact (union-set nil '(2 4)) =>
       '(2 4))
