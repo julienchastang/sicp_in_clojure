20dfbd7ad5b2d915729110bde04838deb994509c
Julien Chastang <julien.c.chastang@gmail.com>
2011-09-26 22:43:07 -0600

    Moving to clojure 1.3 and now using clojure-jack-in

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject sicp_in_clojure "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :description "SICP in Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]
 		 [com.lowagie/itext "2.0.7"]
-		 [midje "1.1-RC1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :disable-implicit-clean true)
+		 [midje "1.2.0"]])
