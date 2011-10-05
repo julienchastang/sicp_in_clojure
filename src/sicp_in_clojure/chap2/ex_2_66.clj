6b1d06e4d6ca0e06e33e6fcb17bb1d01435a91cc
Julien Chastang <julien.c.chastang@gmail.com>
2011-10-04 23:37:07 -0600

    Exercise 2.66.

---
+++ src/sicp_in_clojure/chap2/ex_2_66.clj
@@ -0,0 +1,12 @@
+(ns sicp-in-clojure.chap2.ex-2-66
+    (:use [sicp-in-clojure.chap2.ex-2-63]))
+
+(defn lookup [key set]
+  (cond (empty? set) false
+        (= key (first (entry set))) (entry set)
+        (< key (first (entry set))) (lookup key (left-branch set))
+        (> key (first (entry set))) (lookup key (right-branch set))))
+
+(def b '((1 'a) () ((2 'b) () ((3 'c) () ()))))
+
+(lookup 3 b)
