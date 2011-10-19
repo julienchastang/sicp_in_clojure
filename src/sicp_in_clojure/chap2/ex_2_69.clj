04894ef386b7329d85abfdb14db659a9444016d5
Julien Chastang <julien.c.chastang@gmail.com>
2011-10-19 00:01:53 -0600

    Cleaning up 2.69

--- src/sicp_in_clojure/chap2/ex_2_69.clj
+++ src/sicp_in_clojure/chap2/ex_2_69.clj
@@ -1,20 +1,15 @@
 (ns sicp-in-clojure.chap2.ex-2-69
   (:use [sicp-in-clojure.chap2.ex-2-67]))
 
-(defn successive-merge [s]
-  (letfn [(f [[s w]] (make-leaf s w))]
-    (if (= (count s) 1)
-      (f (last s))
-      (make-code-tree (f (first s)) (successive-merge (rest s))))))
 
 (defn successive-merge [s]
-  (if (= (count s) 1)
-    (last s)
+  (if (empty? (rest s))
+    (first s)
     (make-code-tree (first s) (successive-merge (rest s)))))
 
 (defn generate-huffman-tree [pairs]
   (successive-merge (make-leaf-set pairs)))
 
-(def a '((A 5) (B 3)))
+(def a '((A 5) (B 3) (C 1)))
 
 (generate-huffman-tree a)
