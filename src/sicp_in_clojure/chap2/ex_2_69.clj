67f14c19a3184a23c77b26add8240c95f3b27ae6
Julien Chastang <julien.c.chastang@gmail.com>
2011-10-21 15:40:47 -0600

    Fixing bug in 2.69

--- src/sicp_in_clojure/chap2/ex_2_69.clj
+++ src/sicp_in_clojure/chap2/ex_2_69.clj
@@ -1,5 +1,6 @@
 (ns sicp-in-clojure.chap2.ex-2-69
-  (:use [sicp-in-clojure.chap2.ex-2-67]))
+  (:use [sicp-in-clojure.chap2.ex-2-67])
+  (:use [sicp-in-clojure.chap2.ex-2-68]))
 
 
 (defn successive-merge [s]
@@ -8,8 +9,15 @@
     (make-code-tree (first s) (successive-merge (rest s)))))
 
 (defn generate-huffman-tree [pairs]
-  (successive-merge (make-leaf-set pairs)))
+  (successive-merge (reverse (make-leaf-set pairs))))
 
 (def a '((A 5) (B 3) (C 1)))
 
 (generate-huffman-tree a)
+
+(def pairs '((A 8) (B 3) (C 1) (D 1) (E 1) (F 1) (G 1) (H 1)))
+
+(generate-huffman-tree pairs)
+
+(encode '(A) (generate-huffman-tree pairs))
+
