12d79540640c542170b7f1baa9970cb4cc9f1949
Julien Chastang <julien.c.chastang@gmail.com>
2011-10-23 23:47:52 -0600

    Making sure I can do the round trip

--- src/sicp_in_clojure/chap2/ex_2_69.clj
+++ src/sicp_in_clojure/chap2/ex_2_69.clj
@@ -11,9 +11,13 @@
 (defn generate-huffman-tree [pairs]
   (successive-merge (reverse (make-leaf-set pairs))))
 
-(def a '((A 5) (B 3) (C 1)))
+(def a '((A 4) (B 2) (D 1) (C 1)))
 
 (generate-huffman-tree a)
+
+(encode '(A D A B B C A) (generate-huffman-tree a))
+
+;; (0 1 1 0 0 1 0 1 0 1 1 1 0) 
 
 (def pairs '((A 8) (B 3) (C 1) (D 1) (E 1) (F 1) (G 1) (H 1)))
 
